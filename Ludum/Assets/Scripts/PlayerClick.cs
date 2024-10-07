e35c996ef48c03f573ccfbd32da4567aac1566d2
Ormp <communists.1909@mail.ru>
2024-10-07 19:15:55 +0300

    Fix№5

--- Ludum/Assets/Scripts/PlayerClick.cs
+++ Ludum/Assets/Scripts/PlayerClick.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class PlayerClick : MonoBehaviour
 {
-    public GameObject enemy; 
-
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) 
+        if (Input.GetMouseButtonDown(0))
         {
-            EnemyStats enemyStats = enemy.GetComponent<EnemyStats>();
-            enemyStats.health -= 1; 
-            Debug.Log("Enemy Health: " + enemyStats.health);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+            if (hit.collider != null && hit.collider.CompareTag("Enemy"))
+            {
+                EnemyStats enemyStats = hit.collider.GetComponent<EnemyStats>();
+                if (enemyStats != null)
+                {
+                    enemyStats.health -= 1;
+                    Debug.Log("Enemy Health: " + enemyStats.health);
+
+                    
+                    if (enemyStats.health <= 0)
+                    {
+                        Destroy(hit.collider.gameObject);
+                    }
+                }
+            }
         }
     }
 }
