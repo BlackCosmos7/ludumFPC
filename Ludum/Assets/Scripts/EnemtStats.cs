d15a66021d6bf74d0230baefbae26c942259c0a9
BlackCosmos7 <xenia.ksu2018@yandex.ru>
2024-10-08 20:40:37 +0300

    2

--- Ludum/Assets/Scripts/EnemtStats.cs
+++ Ludum/Assets/Scripts/EnemtStats.cs
@@ -8,21 +8,12 @@ public class EnemyStats : MonoBehaviour
     public float health = 3;
     public float damage = 1;
 
-void Update()
-    {
-        if (health <= 0)
-        {
-            Destroy(gameObject);
-        }
-    }
-
     public void TakeDamage(float amount)
     {
         health -= amount;
-
-        if (health < 0)
+        if (health <= 0)
         {
-            health = 0;
+            Destroy(gameObject);
         }
     }
 }
--- Ludum/Assets/Scripts/PlayerClick.cs
+++ Ludum/Assets/Scripts/PlayerClick.cs
@@ -6,22 +6,16 @@ public class PlayerClick : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
 
             if (hit.collider != null && hit.collider.CompareTag("Enemy"))
             {
                 EnemyStats enemyStats = hit.collider.GetComponent<EnemyStats>();
                 if (enemyStats != null)
                 {
-                    enemyStats.health -= 1;
+                    enemyStats.TakeDamage(1); // Используйте метод TakeDamage
                     Debug.Log("Enemy Health: " + enemyStats.health);
-
-                    
-                    if (enemyStats.health <= 0)
-                    {
-                        Destroy(hit.collider.gameObject);
-                    }
                 }
             }
         }
